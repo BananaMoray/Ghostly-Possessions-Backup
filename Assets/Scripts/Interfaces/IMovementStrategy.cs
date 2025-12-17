667925310ff121de48c9b9fc4b11a161a2c9310d
BananaMoray <freyakropsch@gmail.com>
2025-12-17 12:10:54 +0100

    New models + Walrus

--- Assets/Scripts/Interfaces/IMovementStrategy.cs
+++ Assets/Scripts/Interfaces/IMovementStrategy.cs
@@ -2,6 +2,16 @@ using UnityEngine;
 
 public interface IMovementStrategy
 {
+    //Rigidbody Rigidbody { get; set; }
+
+    //float MaxSpeed { get; set; }
+
+    //float Acceleration {  get; set; }
+
+    //float Deceleration {  get; set; }
+
+    //Vector3 CurrentVelocity { get; set; }
+
     void Move(Vector3 targetPosition, Rigidbody rb, float maxSpeed, float acceleration, float deceleration, Vector3 currentVelocity);
 
 }
--- Assets/Scripts/MovementStrategies/FollowPlayerStrategy.cs
+++ Assets/Scripts/MovementStrategies/FollowPlayerStrategy.cs
@@ -5,7 +5,6 @@ public class FollowPlayerStrategy : MonoBehaviour, IMovementStrategy
     [SerializeField]
     private float _minDistance = 10f;
 
-    Vector3 desiredVelocity = Vector3.zero;
     Vector3 newVelocity = Vector3.zero;
 
     public void Move(Vector3 targetPosition, Rigidbody rb, float maxSpeed, float acceleration, float deceleration, Vector3 currentVelocity)
@@ -15,20 +14,7 @@ public class FollowPlayerStrategy : MonoBehaviour, IMovementStrategy
 
         Vector3 moveDirection = Vector3.zero;
 
-        //i have to square my mininum distance here, since the magnitude is also squared
-        //if ((targetPosition - transform.position).sqrMagnitude > _minDistance * _minDistance)
-        //{
-        //    moveDirection = (targetPosition - transform.position).normalized;
-        //    //Debug.Log();
-        //}
-        //else
-        //    moveDirection = Vector3.zero;
-
         moveDirection = (targetPosition - transform.position).normalized;
-        //Debug.Log();
-
-        desiredVelocity = moveDirection * maxSpeed;
-         
 
         if (moveDirection.sqrMagnitude > 0.01f)
         {
